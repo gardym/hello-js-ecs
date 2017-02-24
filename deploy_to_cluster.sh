eedae68fd41d7046474a1b130e622386e5340b42
Mike Gardiner <mike@gardym.io>
2017-02-24 12:18:02 -0600

    Add the new task definition and scale it in one command

--- deploy_to_cluster.sh
+++ deploy_to_cluster.sh
@@ -18,11 +18,12 @@ aws ecs update-service --service $SVC_ARN --desired-count 0
 TASK_ID=$(aws ecs list-tasks | jaq "taskArns[0]")
 aws ecs stop-task --task $TASK_ID
 
-# Update the service to use the new taskDefinition
-aws ecs update-service --service $SVC_ARN --task-definition $NEW_TD_ARN
-
-# Scale the service back up to 1
-aws ecs update-service --service $SVC_ARN --desired-count 1
+# Update the service to use the new taskDefinition and
+# scale it back up to 1 task
+aws ecs update-service \
+  --service $SVC_ARN \
+  --task-definition $NEW_TD_ARN \
+  --desired-count 1
 
 # Deregister the old task definition
 aws ecs deregister-task-definition --task-definition $OLD_TD_ARN
