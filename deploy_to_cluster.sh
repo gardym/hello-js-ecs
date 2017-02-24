d757c4edb99c877b9cf1e5a1041e201b5ca9edc3
Mike Gardiner <mike@gardym.io>
2017-02-24 13:30:25 -0600

    Restict operations to the defined cluter, be polite

--- deploy_to_cluster.sh
+++ deploy_to_cluster.sh
@@ -10,8 +10,7 @@ sed -e "s#VERSION#$VERSION#g" \
   -e "s#ECS_FAMILY#$ECS_FAMILY#g" \
   $TD_TEMPLATE > $TD_COMPOSED
 
-OLD_TD_ARN=$(aws ecs list-task-definitions | jaq "taskDefinitionArns[0]")
-SVC_ARN=$(aws ecs list-services | jaq "serviceArns[0]")
+SVC_ARN=$(aws ecs list-services --cluster $CLUSTER_NAME | jaq "serviceArns[0]")
 
 # Push new taskDefinition
 NEW_TD_ARN=$(aws ecs register-task-definition \
@@ -20,7 +19,7 @@ NEW_TD_ARN=$(aws ecs register-task-definition \
 
 # Stop running tasks
 aws ecs update-service --service $SVC_ARN --desired-count 0
-TASK_ID=$(aws ecs list-tasks | jaq "taskArns[0]")
+TASK_ID=$(aws ecs list-tasks --cluster $CLUSTER_NAME | jaq "taskArns[0]")
 aws ecs stop-task --task $TASK_ID
 
 # Update the service to use the new taskDefinition and
@@ -29,6 +28,3 @@ aws ecs update-service \
   --service $SVC_ARN \
   --task-definition $NEW_TD_ARN \
   --desired-count 1
-
-# Deregister the old task definition
-aws ecs deregister-task-definition --task-definition $OLD_TD_ARN
